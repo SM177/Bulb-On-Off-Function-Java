1f8408bd5bdbe6d4f47f119c3cfecacaf251aaf3
Syed Moiz <118743795+SM177@users.noreply.github.com>
2022-12-02 12:13:45 +0500

    Add files via upload

---
+++ Bulb.js
@@ -0,0 +1,15 @@
+// Button On Setting
+
+function bulbon()
+{
+document.getElementById("bulb").src="bulb-on.png";
+}
+
+
+// Button Off Setting
+
+
+function bulboff()
+{
+document.getElementById("bulb").src="bulb-off.png";
+}
